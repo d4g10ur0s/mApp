d7aa93d02cc088996310e0d774ad58d5efa6ee04
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2020-08-04 02:26:49 +0300

    Update katiApp.py

--- katiApp.py
+++ katiApp.py
@@ -31,33 +31,72 @@ class mGrid(GridLayout):
         self.sgn = sButton()
         #log in button
         self.lg = lButton()
+        self.sgn.bind(on_press=self.SGCallback)
         self.lg.bind(on_press=self.LGCallback)
 
         self.add_widget(self.sgn)
         self.add_widget(self.lg)
 
+        #to LogIn
     def LGCallback(self, instance, *pos):
         self.remove_widget(self.lg)
         self.remove_widget(self.sgn)
         self.cols = None
-        self.rows = 2
-
+        self.rows = 5
+###############################################################################
         #username label
-        usrname = BackgroundLabel(text = 'Username', font_size = '25dp')
-        usrname.background_color = [0,.1,1,.5]
+        usrname = BackgroundLabel(text='Username', font_size='25dp')
+        usrname.background_color=[1,.1,0,.5]
         self.add_widget(usrname)
         #text input for username
         self.lg = None
-        self.lg = TextInput(multiline = False)
+        self.lg = TextInput(multiline=False)
+        self.add_widget(self.lg)
+        #Password label
+        pssw=BackgroundLabel(text='Password',font_size='25dp')
+        pssw.background_color=[1,.1,0,.5]
+        self.add_widget(pssw)
+        #text input for Password
+        self.sgn = TextInput(multiline=False)
+        self.add_widget(self.sgn)
+        #Confirm Button
+        cnfr=Button(text='Confirm',font_size='25dp',background_color=[1,.1,0,.5])
+        self.add_widget(cnfr)
+
+        #To SignIn
+    def SGCallback(self, instance, *pos):
+        self.remove_widget(self.lg)
+        self.remove_widget(self.sgn)
+        self.cols = None
+        self.rows = 7
+###############################################################################
+        #username label
+        usrname = BackgroundLabel(text = 'Username', font_size = '25dp')
+        usrname.background_color = [0,.2,1,.7]
+        self.add_widget(usrname)
+        #text input for username
+        self.lg = None
+        self.lg = TextInput(multiline = False,cursor_width = '25dp')
         self.add_widget(self.lg)
         #Password Label
         pssw = BackgroundLabel(text = 'Password' , font_size = '25dp')
-        pssw.background_color = [0,.1,1,.5]
+        pssw.background_color = [0,.2,1,.7]
         self.add_widget(pssw)
         #text input for Password
         self.sgn = None
         self.sgn = TextInput(multiline = False)
         self.add_widget(self.sgn)
+        #Password Confirmation
+        pssw1 = BackgroundLabel(text = 'Password Confirmation' , font_size = '25dp')
+        pssw1.background_color = [0,.2,1,.7]
+        self.add_widget(pssw1)
+        #text input for confirmation
+        psswc = None
+        psswc = TextInput(multiline = False)
+        self.add_widget(psswc)
+        #Confirmation button
+        conf = Button(text='Confirm', font_size='25dp', background_color=[0,.1,1,.5])
+        self.add_widget(conf)
 
 
 class katiApp(App):
